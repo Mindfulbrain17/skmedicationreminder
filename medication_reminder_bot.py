419dfc9cbf1a377fc4fd6f2577266e3c7c1da465
AG <87939949+Anant-Goyal@users.noreply.github.com>
2025-02-05 11:12:34 +0530

    Update medication_reminder_bot.py

--- medication_reminder_bot.py
+++ medication_reminder_bot.py
@@ -8,6 +8,7 @@ CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 
 # Evening reminder messages (Random Generated Messages)
 EVENING_MESSAGES = [
+   "Swati Kulshrestha aur Anant Goyal, kya khana banana start kiya ya abhi bhi recipe dhoond rahe ho?",  
    "AK aur SK, dinner ho gaya ya abhi bhi ‘Kuch light kha lete hain’ mood mein ho?", 
    "Swati aur Anant, khana kha liya ya firse ‘Bas 5 minute aur’ chal raha hai?",  
    "Anant Kulshrestha aur Swati Goyal, kya aaj bhi khana dekh ke ‘Kal se pakka diet’ wala promise kiya hai?",  
